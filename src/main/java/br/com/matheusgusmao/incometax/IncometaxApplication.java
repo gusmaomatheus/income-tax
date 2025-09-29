ac9a18f9aaac0d8f3415a44278c2bdc1cf08c22f
Matheus <mgusmao.dev@gmail.com>
2025-09-28 23:21:04 -0300

    chore: init

---
+++ src/main/java/br/com/matheusgusmao/incometax/IncometaxApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusgusmao.incometax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncometaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncometaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheusgusmao/incometax/IncometaxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusgusmao.incometax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncometaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
