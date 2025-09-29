9aeac0e810c7901cd0149da21f69d51473380872
Matheus <mgusmao.dev@gmail.com>
2025-09-28 23:29:43 -0300

    chore: fix typo in main class name

--- src/main/java/br/com/matheusgusmao/incometax/IncomeTaxApplication.java
+++ src/main/java/br/com/matheusgusmao/incometax/IncomeTaxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IncometaxApplication {
+public class IncomeTaxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IncometaxApplication.class, args);
+		SpringApplication.run(IncomeTaxApplication.class, args);
 	}
 
 }
