a66c397c278bfd110eb763b2509fbaf5d20c5ac3
Matheus <mgusmao.dev@gmail.com>
2025-09-29 01:32:47 -0300

    feat(security/jwt): create JWT validation middleware

---
+++ src/main/java/br/com/matheusgusmao/incometax/infra/security/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package br.com.matheusgusmao.incometax.infra.security.config.jwt;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String tokenPrefix = "Bearer ";
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(tokenPrefix)) {
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        final String token = authorizationHeader.substring(tokenPrefix.length());
+
+        try {
+            final String email = jwtService.getSubject(token);
+
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
+
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+
+        } catch (JwtException exception) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"message\": \"JWT is not valid or is expired.\"}");
+
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
