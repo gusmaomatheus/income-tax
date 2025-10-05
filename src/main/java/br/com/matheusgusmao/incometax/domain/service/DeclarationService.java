feb50adfc2fc70d874acdf4e794eb206d91b5464
Matheus <mgusmao.dev@gmail.com>
2025-10-05 18:52:08 -0300

    feat(domain/service): implement the logic of creating a new Declaration

--- src/main/java/br/com/matheusgusmao/incometax/domain/service/DeclarationService.java
+++ src/main/java/br/com/matheusgusmao/incometax/domain/service/DeclarationService.java
@@ -1,13 +1,25 @@
 package br.com.matheusgusmao.incometax.domain.service;
 
 import br.com.matheusgusmao.incometax.domain.model.declaration.Declaration;
+import br.com.matheusgusmao.incometax.domain.repository.DeclarationRepository;
+import br.com.matheusgusmao.incometax.infra.exception.custom.EntityAlreadyExistsException;
 
 import java.util.UUID;
 
 public class DeclarationService {
 
+    private final DeclarationRepository declarationRepository;
+
+    public DeclarationService(final DeclarationRepository declarationRepository) {
+        this.declarationRepository = declarationRepository;
+    }
+
     public Declaration createNewDeclaration(UUID taxpayerId, int year) {
-        // TODO...
-        return null;
+        if (declarationRepository.existsByTaxpayerIdAndYear(taxpayerId, year)) {
+            throw new EntityAlreadyExistsException("A declaration for the given taxpayer and year already exists.");
+        }
+
+        Declaration newDeclaration = new Declaration(taxpayerId, year);
+        return declarationRepository.save(newDeclaration);
     }
 }
