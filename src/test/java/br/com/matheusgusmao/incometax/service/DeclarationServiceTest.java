58ddedc9832a98ace2e501c4dc47c9eaccb402b2
Matheus <mgusmao.dev@gmail.com>
2025-10-05 19:13:38 -0300

    test(service): adjust tests after changes in the repository

--- src/test/java/br/com/matheusgusmao/incometax/service/DeclarationServiceTest.java
+++ src/test/java/br/com/matheusgusmao/incometax/service/DeclarationServiceTest.java
@@ -1,16 +1,17 @@
 package br.com.matheusgusmao.incometax.service;
 
 import br.com.matheusgusmao.incometax.domain.model.declaration.Declaration;
-import br.com.matheusgusmao.incometax.domain.model.declaration.DeclarationStatus;
-import br.com.matheusgusmao.incometax.domain.repository.DeclarationRepository;
 import br.com.matheusgusmao.incometax.domain.service.DeclarationService;
 import br.com.matheusgusmao.incometax.infra.exception.custom.EntityAlreadyExistsException;
+import br.com.matheusgusmao.incometax.infra.persistence.entity.declaration.DeclarationEntity;
+import br.com.matheusgusmao.incometax.infra.persistence.mapper.DeclarationMapper;
+import br.com.matheusgusmao.incometax.infra.persistence.repository.DeclarationRepository;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
@@ -25,45 +26,45 @@ public class DeclarationServiceTest {
     @Mock
     private DeclarationRepository declarationRepository;
 
+    @Spy
+    private DeclarationMapper declarationMapper = new DeclarationMapper();
+
     @InjectMocks
     private DeclarationService declarationService;
 
     @Test
-    @Tag("TDD")
-    @Tag("UnitTest")
     @DisplayName("[Scenario] Should create new declaration successfully")
     void shouldCreateNewDeclarationSuccessfullyWhenNonExistent() {
         final UUID taxpayerId = UUID.randomUUID();
         final int year = 2025;
-        final Declaration savedDeclaration = new Declaration(1L, taxpayerId, year, DeclarationStatus.EDITING);
 
         when(declarationRepository.existsByTaxpayerIdAndYear(taxpayerId, year)).thenReturn(false);
-        when(declarationRepository.save(any(Declaration.class))).thenReturn(savedDeclaration);
+        when(declarationRepository.save(any(DeclarationEntity.class))).thenAnswer(invocation -> {
+            DeclarationEntity entity = invocation.getArgument(0);
+            entity.setId(1L);
+            return entity;
+        });
 
         Declaration result = declarationService.createNewDeclaration(taxpayerId, year);
 
         assertNotNull(result);
         assertNotNull(result.getId());
         assertEquals(1L, result.getId());
-        assertEquals(taxpayerId, result.getTaxpayerId());
-        assertEquals(year, result.getYear());
-        assertEquals(DeclarationStatus.EDITING, result.getStatus());
-
         verify(declarationRepository).existsByTaxpayerIdAndYear(taxpayerId, year);
-        verify(declarationRepository).save(any(Declaration.class));
+        verify(declarationRepository).save(any(DeclarationEntity.class));
     }
 
     @Test
-    @Tag("TDD")
-    @Tag("UnitTest")
     @DisplayName("[Scenario] Should prevent duplicate declaration in the same year")
     void shouldThrowExceptionWhenDeclarationForSameYearAlreadyExists() {
         final UUID taxpayerId = UUID.randomUUID();
         final int year = 2025;
         when(declarationRepository.existsByTaxpayerIdAndYear(taxpayerId, year)).thenReturn(true);
 
-        assertThrows(EntityAlreadyExistsException.class, () -> declarationService.createNewDeclaration(taxpayerId, year));
+        assertThrows(EntityAlreadyExistsException.class,
+                () -> declarationService.createNewDeclaration(taxpayerId, year)
+        );
 
-        verify(declarationRepository, never()).save(any(Declaration.class));
+        verify(declarationRepository, never()).save(any(DeclarationEntity.class));
     }
 }
