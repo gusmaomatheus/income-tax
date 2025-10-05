563b12da7970c0e7266b6f23ea2bad2c6efa9681
Matheus <mgusmao.dev@gmail.com>
2025-10-05 18:38:09 -0300

    feat(domain/service): create domain layer service for use cases involving the Declaration model

---
+++ src/main/java/br/com/matheusgusmao/incometax/domain/service/DeclarationService.java
@@ -0,0 +1,13 @@
+package br.com.matheusgusmao.incometax.domain.service;
+
+import br.com.matheusgusmao.incometax.domain.model.declaration.Declaration;
+
+import java.util.UUID;
+
+public class DeclarationService {
+
+    public Declaration createNewDeclaration(UUID taxpayerId, int year) {
+        // TODO...
+        return null;
+    }
+}
