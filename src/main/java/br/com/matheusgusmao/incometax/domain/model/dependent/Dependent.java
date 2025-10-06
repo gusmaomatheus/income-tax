6b06bb410bba58f26e25541d3736ac867821d75f
LucasLotumolo <lucasblotumolo@gmail.com>
2025-10-06 20:30:09 -0300

    refactor: folder adjustment and standardization with class declaration

--- src/main/java/br/com/matheusgusmao/incometax/domain/model/dependent/Dependent.java
+++ src/main/java/br/com/matheusgusmao/incometax/domain/model/dependent/Dependent.java
@@ -1,26 +1,26 @@
 package br.com.matheusgusmao.incometax.domain.model.declaration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class Dependent {
     private Long id;
     private String name;
-    private String cpf;
+    private Cpf cpf;
     private LocalDate birthDate;
 
-    public Dependent(Long id, String name, String cpf, LocalDate birthDate) {
+    public Dependent(Long id, String name, Cpf cpf, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
         this.birthDate = birthDate;
     }
 
-    public Dependent(String name, String cpf, LocalDate birthDate) {
+    public Dependent(String name, Cpf cpf, LocalDate birthDate) {
         this(null, name, cpf, birthDate);
     }
-
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getCpf() { return cpf; }
-    public LocalDate getBirthDate() { return birthDate; }
 }
