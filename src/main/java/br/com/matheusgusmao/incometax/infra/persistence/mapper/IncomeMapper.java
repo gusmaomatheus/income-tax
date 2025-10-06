2e78a8bcb0d3863da99c298b49aec274960d471c
MuriloCSS <murilosao06@gmail.com>
2025-10-06 13:57:56 -0300

    feat(infra): implement IncomeMapper functions

--- src/main/java/br/com/matheusgusmao/incometax/infra/persistence/mapper/IncomeMapper.java
+++ src/main/java/br/com/matheusgusmao/incometax/infra/persistence/mapper/IncomeMapper.java
@@ -1,4 +1,25 @@
 package br.com.matheusgusmao.incometax.infra.persistence.mapper;
 
+import br.com.matheusgusmao.incometax.domain.model.income.Income;
+import br.com.matheusgusmao.incometax.infra.persistence.entity.declaration.DeclarationEntity;
+import br.com.matheusgusmao.incometax.infra.persistence.entity.income.IncomeEntity;
+
 public class IncomeMapper {
+    public IncomeEntity toEntity(Income domain, DeclarationEntity declarationEntity) {
+        if (domain == null) return null;
+
+        IncomeEntity entity = new IncomeEntity();
+        entity.setId(domain.getId());
+        entity.setPayingSource(domain.getPayingSource());
+        entity.setType(domain.getType());
+        entity.setValue(domain.getValue());
+        entity.setDeclaration(declarationEntity);
+        return entity;
+    }
+
+    public Income toDomain(IncomeEntity entity) {
+        if (entity == null) return null;
+
+        return new Income(entity.getId(), entity.getPayingSource(), entity.getType(), entity.getValue());
+    }
 }
