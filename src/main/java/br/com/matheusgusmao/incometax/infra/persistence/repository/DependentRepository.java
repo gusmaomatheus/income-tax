4ba7c02c37ce14bbdf21dedd3bcaf7fc279d7b97
LucasLotumolo <lucasblotumolo@gmail.com>
2025-10-05 23:49:57 -0300

    feat: implementing domain to represent a dependent model

---
+++ src/main/java/br/com/matheusgusmao/incometax/infra/persistence/repository/DependentRepository.java
@@ -0,0 +1,10 @@
+package br.com.matheusgusmao.incometax.infra.persistence.repository;
+
+import br.com.matheusgusmao.incometax.infra.persistence.entity.declaration.DependentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DependentRepository extends JpaRepository<DependentEntity, Long> {
+    Optional<DependentEntity> findByCpf(String cpf);
+}
