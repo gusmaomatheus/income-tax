99fd01c84a5df9a9c0af427f13d09804e31e20cd
MuriloCSS <murilosao06@gmail.com>
2025-10-06 13:29:39 -0300

    feat(infra): implement IncomeEntity class

--- src/main/java/br/com/matheusgusmao/incometax/infra/persistence/entity/income/IncomeEntity.java
+++ src/main/java/br/com/matheusgusmao/incometax/infra/persistence/entity/income/IncomeEntity.java
@@ -1,5 +1,34 @@
 package br.com.matheusgusmao.incometax.infra.persistence.entity.income;
 
+import br.com.matheusgusmao.incometax.domain.model.income.IncomeType;
+import br.com.matheusgusmao.incometax.infra.persistence.entity.declaration.DeclarationEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "incomes")
+@Getter
+@Setter
 public class IncomeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String payingSource;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private IncomeType type;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal value;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "declaration_id", nullable = false)
+    private DeclarationEntity declaration;
 }
