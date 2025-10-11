036b13c6e2b3fd3c6483d7e6cde1c85b516e116d
Matheus <mgusmao.dev@gmail.com>
2025-10-10 21:36:16 -0300

    feat: enhance dependent management endpoints
    - Refactor DependentController with improved documentation
    - Add Swagger annotations for better API documentation
    - Simplify controller to focus on adding dependents

--- src/main/java/br/com/matheusgusmao/incometax/web/controller/DependentController.java
+++ src/main/java/br/com/matheusgusmao/incometax/web/controller/DependentController.java
@@ -3,11 +3,16 @@ package br.com.matheusgusmao.incometax.web.controller;
 import br.com.matheusgusmao.incometax.domain.service.DependentService;
 import br.com.matheusgusmao.incometax.web.dto.dependent.CreateDependentRequest;
 import br.com.matheusgusmao.incometax.web.dto.dependent.DependentResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/declarations/{declarationId}/dependents") 
+@RequestMapping("/declarations/{declarationId}/dependents")
+@Tag(name = "Dependent Management")
 public class DependentController {
 
     private final DependentService dependentService;
@@ -16,11 +21,18 @@ public class DependentController {
         this.dependentService = dependentService;
     }
 
+    @Operation(summary = "Add dependent to declaration")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Dependent added successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid CPF or duplicate dependent"),
+            @ApiResponse(responseCode = "404", description = "Declaration not found"),
+            @ApiResponse(responseCode = "401", description = "Authentication required")
+    })
     @PostMapping
     public ResponseEntity<DependentResponse> addDependent(
             @PathVariable Long declarationId,
             @RequestBody CreateDependentRequest request) {
-        DependentResponse response = dependentService.addDependent(declarationId, request);
+        var response = dependentService.addDependent(declarationId, request);
         return ResponseEntity.status(201).body(response);
     }
 }
