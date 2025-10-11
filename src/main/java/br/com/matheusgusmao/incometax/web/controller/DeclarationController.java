3f50e0c3b1c182380e03950ac3b21cee83249d3c
Matheus <mgusmao.dev@gmail.com>
2025-10-10 21:56:39 -0300

    feat(web): add validations for requests involving declarations

--- src/main/java/br/com/matheusgusmao/incometax/web/controller/DeclarationController.java
+++ src/main/java/br/com/matheusgusmao/incometax/web/controller/DeclarationController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -58,7 +59,7 @@ public class DeclarationController {
             )
     })
     @PostMapping
-    public ResponseEntity<DeclarationResponse> create(@RequestBody CreateDeclarationRequest request, @AuthenticationPrincipal UserEntity authenticatedUser) {
+    public ResponseEntity<DeclarationResponse> create(@Valid @RequestBody CreateDeclarationRequest request, @AuthenticationPrincipal UserEntity authenticatedUser) {
 
         var taxpayerId = authenticatedUser.getId();
 
--- src/main/java/br/com/matheusgusmao/incometax/web/dto/declaration/CreateDeclarationRequest.java
+++ src/main/java/br/com/matheusgusmao/incometax/web/dto/declaration/CreateDeclarationRequest.java
@@ -1,4 +1,6 @@
 package br.com.matheusgusmao.incometax.web.dto.declaration;
 
-public record CreateDeclarationRequest(int year) {
+import jakarta.validation.constraints.NotNull;
+
+public record CreateDeclarationRequest(@NotNull(message = "Year cannot be null") int year) {
 }
