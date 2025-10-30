9cf87a3e724890a487063dc7e26fabdbab21fe55
MuriloCSS <murilosao06@gmail.com>
2025-10-30 18:43:58 -0300

    test(expense): add test to accept positive value

--- src/test/java/br/com/matheusgusmao/incometax/domain/model/expense/DeductibleExpenseDomainTest.java
+++ src/test/java/br/com/matheusgusmao/incometax/domain/model/expense/DeductibleExpenseDomainTest.java
@@ -96,5 +96,12 @@ public class DeductibleExpenseDomainTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("Expense value must be positive");
         }
+        @Test
+        @DisplayName("Should accept positive value")
+        void shouldAcceptPositiveValue() {
+            var expense = new DeductibleExpense("Health Plan", ExpenseType.HEALTH, BigDecimal.valueOf(100));
+
+            assertThat(expense.getValue()).isPositive();
+        }
     }
 }
