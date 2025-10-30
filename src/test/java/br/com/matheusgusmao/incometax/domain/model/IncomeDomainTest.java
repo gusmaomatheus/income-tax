f57dbadd12c74fb10da9a219e0e88714cae54a42
MuriloCSS <murilosao06@gmail.com>
2025-10-30 10:46:28 -0300

    test(income): add test to create income with valid parameters

--- src/test/java/br/com/matheusgusmao/incometax/domain/model/IncomeDomainTest.java
+++ src/test/java/br/com/matheusgusmao/incometax/domain/model/IncomeDomainTest.java
@@ -1,6 +1,38 @@
 package br.com.matheusgusmao.incometax.domain.model;
+import br.com.matheusgusmao.incometax.domain.model.income.Income;
+import br.com.matheusgusmao.incometax.domain.model.income.IncomeType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Income Domain - Structural Tests")
+@Tag("Structural")
+@Tag("UnitTest")
+@Tag("Unit")
 public class IncomeDomainTest {
+    @Nested
+    @DisplayName("Income Construction")
+    class IncomeConstructionTests {
 
+        @Test
+        @DisplayName("Should create income with valid parameters")
+        void shouldCreateIncomeWithValidParameters() {
+            var payingSource = "Company XYZ";
+            var type = IncomeType.SALARY;
+            var value = BigDecimal.valueOf(5000);
 
+            var income = new Income(payingSource, type, value);
+
+            assertThat(income.getPayingSource()).isEqualTo(payingSource);
+            assertThat(income.getType()).isEqualTo(type);
+            assertThat(income.getValue()).isEqualTo(value);
+            assertThat(income.getId()).isNull();
+        }
+    }
 }
