53a9d3c8a3a6f4958ecb6489d6bc5dabbb39d96e
MuriloCSS <murilosao06@gmail.com>
2025-10-30 17:53:08 -0300

    test(expense): add test to create expense with valid parameters

--- src/test/java/br/com/matheusgusmao/incometax/domain/model/expense/DeductibleExpenseDomainTest.java
+++ src/test/java/br/com/matheusgusmao/incometax/domain/model/expense/DeductibleExpenseDomainTest.java
@@ -1,4 +1,37 @@
 package br.com.matheusgusmao.incometax.domain.model.expense;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("DeductibleExpense Domain - Structural Tests")
+@Tag("Structural")
+@Tag("UnitTest")
+@Tag("Unit")
 public class DeductibleExpenseDomainTest {
+    @Nested
+    @DisplayName("DeductibleExpense Construction")
+    class DeductibleExpenseConstructionTests {
+
+        @Test
+        @DisplayName("Should create expense with valid parameters")
+        void shouldCreateExpenseWithValidParameters() {
+            var description = "Health Plan";
+            var type = ExpenseType.HEALTH;
+            var value = BigDecimal.valueOf(1200);
+
+            var expense = new DeductibleExpense(description, type, value);
+
+            assertThat(expense.getDescription()).isEqualTo(description);
+            assertThat(expense.getType()).isEqualTo(type);
+            assertThat(expense.getValue()).isEqualTo(value);
+            assertThat(expense.getId()).isNull();
+        }
+    }
 }
