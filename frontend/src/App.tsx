de5345884ceb00cc8ad3004e1bc10166573d4624
Matheus <mgusmao.dev@gmail.com>
2025-11-03 23:27:39 -0300

    feat(front): implement normal and protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 import { DefaultLayout } from './components/layouts/DefaultLayout';
+import { ProtectedLayout } from './components/layouts/ProtectedLayout';
+
+import { DashboardPage } from './pages/DashboardPage';
+import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 
 function App(): React.JSX.Element {
@@ -7,6 +13,11 @@ function App(): React.JSX.Element {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+
+      <Route path="/" element={<ProtectedLayout />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Route>
     </Routes>
   );
